1a97bc6c5ed5d54b17590ff796f39feb55e32990
Jeffery Jones <56287988+jeff-jones2020@users.noreply.github.com>
2020-05-18 15:35:57 -0700

    added missing files from forked repo

---
+++ drills02/yourcode.js
@@ -0,0 +1,18 @@
+function countOccurences(wordArray, target){
+  let count = 0;
+
+  wordArray.forEach(item => count += item === target ? 1 : 0);
+  return count;
+}
+
+function wordLengths(){
+
+}
+
+function getMinMaxMean(){
+
+}
+
+function findMode(){
+
+}
